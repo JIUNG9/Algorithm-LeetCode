f21cbab87ed0a3f0060eddb256fa112afad13e6a
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-02-02 12:00:51 +0900

    add: add the TwoSum algorithm

---
+++ src/main/java/com/algo/TwoSum.java
@@ -0,0 +1,24 @@
+package com.algo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    int len = nums.length;
+    for (int i = 0; i < len; i++) {
+      if (map.containsKey(target - nums[i])) {
+        return new int[]{i, map.get(target - nums[i])};
+      } else {
+        map.put(nums[i], i);
+      }
+    }
+    return new int[]{};
+  }
+
+
+
+}
+
